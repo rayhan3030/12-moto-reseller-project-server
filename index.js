dccf74b2571755580dcfaa0fe7c5f5f2b5bc01dc
rayhan3030 <rayhanddd@gmail.com>
2022-11-28 02:51:09 +0600

    My orders route api created for email query on bookings route

--- index.js
+++ index.js
@@ -49,6 +49,13 @@ async function run() {
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
+        // this api for loading data for users from booking collection to see booking list and pay money email wise
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
 
     }
     finally {
