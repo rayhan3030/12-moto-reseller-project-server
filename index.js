d40c834f47629aeaf67844aecf5e5a26ce064cfc
rayhan3030 <rayhanddd@gmail.com>
2022-11-29 02:51:28 +0600

    jwt system added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
+
 const port = process.env.PORT || 5001;
 
 const app = express();
@@ -17,11 +19,33 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//jwt verify function
+function verifyJWT(req, res, next) {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized Access')
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+
 async function run() {
     try {
         const categoryCollection = client.db('motoResellar').collection('categories');
         const productsCollection = client.db('motoResellar').collection('allProducts');
         const bookingsCollection = client.db('motoResellar').collection('bookings');
+        const usersCollection = client.db('motoResellar').collection('users');
 
         // this api is for loading 3 types of bike in the home page
         app.get('/category', async (req, res) => {
@@ -49,12 +73,37 @@ async function run() {
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
         })
-        // this api for loading data for users from booking collection to see booking list and pay money email wise
-        app.get('/bookings', async (req, res) => {
+        // {MyOrders component} this api for loading data for users from booking collection to see booking list and pay money email wise
+        app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const query = { email: email };
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
+        });
+
+        // db te jodi user thake taile jwttoken ta dibe eijonne ei api
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' })
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ acessToken: '' })
+        })
+
+
+        //send logged in users data to mongodb {from signup component} role can be set in future
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+
         })
 
     }
